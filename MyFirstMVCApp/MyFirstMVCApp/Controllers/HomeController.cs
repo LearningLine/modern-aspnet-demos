1ffd066af1c022d4b90afe3eb5fb324a30486ad1
Pierre Nallet <pierrenallet@hotmail.com>
2015-11-09 11:47:08 -0500

    Adds first MVC app.

---
+++ MyFirstMVCApp/MyFirstMVCApp/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+
+namespace MyFirstMVCApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            /// 1 Get data
+            
+            var restaurants = new[] { "5 guys", "Jimmy Jons", "Chipotle" };
+            //ViewBag.Restaurants = restaurants;
+            ViewBag.Something = "hello, this is cool";
+            ViewData.Model = restaurants;
+            var defaultRestaurant = "5 guys";
+            /// 2 Render data
+            //return new ContentResult() {Content="Hello"};
+            return View(restaurants);
+        }
+
+        public ActionResult Help()
+        {
+            return View();
+        }
+        
+    }
+}
